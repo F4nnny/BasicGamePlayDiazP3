07eaf7c03e41c93d9d8f82758bbf358d685c829c
Daniel Diaz <710147@student.dusd.net>
2023-03-16 11:41:02 -0700

    finished and turned in basic gameplay and started challenge 2.
    finished and turned in basic gameplay and started challenge 2.

--- BasicGamePlayDiazP3/Assets/Scripts/GameManager.cs
+++ BasicGamePlayDiazP3/Assets/Scripts/GameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private int score = 0;
+    private int lives = 3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +18,21 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    public void AddLives(int value)
+    {
+        lives += value;
+        if (lives <= 0)
+        {
+            Debug.Log("Game Over");
+            lives = 0;
+        }
+        Debug.Log("Lives = " + lives);
+    }
+    public void AddScore(int value)
+    {
+        score += value;
+        Debug.Log("Score = " + score);
+    }
+
 }
